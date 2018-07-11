e774a3f7f95b3150f4b3d6453719914ea7146626
Marvin Johanning <manimusmuellus@googlemail.com>
2018-07-11 11:26:00 +0200

    Add more comments, add error checks

--- src/gmail.rb
+++ src/gmail.rb
@@ -1,24 +1,58 @@
-require 'serialport'
-require 'gmail'
-require 'fileutils'
-puts File.dirname(__FILE__)
+#!/bin/env ruby
+
+#Check if operating being used it Linux-based
+unless /linux/ =~ RUBY_PLATFORM then
+  puts "ERROR: Only Linux-based operating systems are currently supported"
+  exit
+end
+
+#Requiring all the different gems
+gems = ['serialport', 'gmail', 'fileutils', 'colorize', 'socket']
+gems.each do |gem|
+  require gem
+end
+
+#Checking internet connection to google.com (pinging it)
+begin
+  TCPSocket.new 'google.com', 443
+rescue SocketError
+  puts "ERROR: ".colorize(:red) + "The program was unable to find an active internet connection. " \
+                                  "Please check your internet connection and try again"
+  exit
+end
+
+#Create string to hold the Arduino's serial connection
+@serial_connection = String.new
+
 #Start the bash file, read the file it creates and find out what port the Arduino is connected to
 system("bash #{File.dirname(__FILE__)}/detect_serial.sh > serial")
 File.open("#{File.dirname(__FILE__)}/serial").each_line do |line|
-   @serial_connection = line.slice(0..(line.index(' '))) if line.include?("Arduino")
+  @serial_connection = line.slice(0..(line.index(' '))) if line.include?("Arduino")
 end
 
 #FileUtils.rm("#{File.dirname(__FILE__)}/src/serial")
 
 #Gmail username and password
-gmail = Gmail.connect("username", "password")
+gmail = Gmail.connect("your_username", "your_password")
 
 #count the number of unread messages
-prev_unread = gmail.inbox.count(:unread)
+begin
+  prev_unread = gmail.inbox.count(:unread)
+rescue Net::IMAP::BadResponseError
+  puts "ERROR: ".colorize(:red) + "Connecting to your Gmail account has failed (NET::IMAP::BadResponseError)\n" \
+                                  "Please ensure that you have entered your correct username and password (refer to README for further details)"
+  exit
+end
 
 #Serial port of the Arduino
-puts "Arduino is connected to: #{@serial_connection}"
-port_file = @serial_connection.gsub(' ', '')
+unless @serial_connection.size > 0
+  puts "ERROR: ".colorize(:red) + "No Arduino has been found connected to your computer\n" \
+                                  "Please check if your Arduino is working and has been properly connected"
+  exit
+else
+  puts "Arduino is connected to: #{@serial_connection}"
+  port_file = @serial_connection.gsub(' ', '')
+end
 
 #this must be same as the baud rate set on the Arduino
 #with Serial.begin
